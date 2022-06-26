14a1f3a310539d9c3d562e4d7f470dc7c0542cb1
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-25 23:00:38 -0400

    v

---
+++ components/Layout/index.ts
@@ -0,0 +1,8 @@
+export * from './Home'
+export * from './Layout'
+export * from './LayoutAdmin'
+export * from './LayoutError'
+export * from './Login'
+export * from './Register'
+
+export * from './admin'
